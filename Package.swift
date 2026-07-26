f510bf179ebfa92f6a36589d194637171bffdd2f
crzhu <crrrrdz@gmail.com>
2026-07-26 23:38:57 +0800

    chore: release v1.0

--- Package.swift
+++ Package.swift
@@ -1,6 +1,12 @@
 // swift-tools-version: 6.0
 
+import Foundation
 import PackageDescription
+
+let packageDirectory = URL(fileURLWithPath: #filePath).deletingLastPathComponent()
+let embeddedInfoPlistPath = packageDirectory
+    .appendingPathComponent("Resources/EmbeddedInfo.plist")
+    .path
 
 let package = Package(
     name: "AppleMusicBar",
@@ -16,6 +22,16 @@ let package = Package(
             path: "Sources/AppleMusicBar",
             swiftSettings: [
                 .swiftLanguageMode(.v5)
+            ],
+            linkerSettings: [
+                // Xcode runs SwiftPM executables without an app bundle. Embed only
+                // the AutoFill preference; app-bundle metadata breaks that launch mode.
+                .unsafeFlags([
+                    "-Xlinker", "-sectcreate",
+                    "-Xlinker", "__TEXT",
+                    "-Xlinker", "__info_plist",
+                    "-Xlinker", embeddedInfoPlistPath
+                ])
             ]
         ),
         .testTarget(
